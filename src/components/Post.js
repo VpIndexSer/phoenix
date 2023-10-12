5a044fe464b0bcd46b9396730b50803be02ce8b6
VpIndexSer <princeindexserver@gmail.com>
2023-10-13 01:55:29 +0530

    padding

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,8 +2,10 @@ import React from 'react'
 export const Post = (props) => {
     const { content} = props;
   return (
-    <div>
-        <img src={content.img} alt={content.name} />
+    <>
+    <div className="col-xs-6 col-sm-3 col-md-2 col-lg-2 postcontainer">
+        <img  className="post" src={content.img} alt={content.name} />
     </div>
+    </>
   )
 }
