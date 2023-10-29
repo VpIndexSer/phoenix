6cf23900270336efc8c4689e9fabc70586accdbb
VpIndexSer <princeindexserver@gmail.com>
2023-10-30 01:24:17 +0530

    clg comment

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ export default function Login(props) {
           body: JSON.stringify({email:credentials.email,password:credentials.password}),
           });
           const json=await response.json();
-          console.log(json)
+          // console.log(json)
           if(json.success)
           {
             //redirect
